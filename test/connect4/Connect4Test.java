c28910755c28678ef31260d25b49e13ab192da41
johannes jørgensen <johannesgj@gmail.com>
2012-04-18 20:01:43 +0200

    try with implementing junit tests

---
+++ test/connect4/Connect4Test.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect4;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jgjpro
+ */
+public class Connect4Test {
+    
+    public Connect4Test() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Connect4.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Connect4.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
