553e11c6665f309996ce10b783b07397e208bb82
johannes jørgensen <johannesgj@gmail.com>
2012-04-19 10:29:51 +0200

    implemented junit tests of the grid class

--- src/connect4/Connect4.java
+++ src/connect4/Connect4.java
@@ -23,72 +23,68 @@ public class Connect4 {
         Grid grid = new Grid();
 
         Scanner input = new Scanner(System.in);
-        
-        if (args.length > 0 && args[0].equals("test")) {//if cli arguments is test then
-            Test test = new Test();
-        } else {
-            while (state != -1) {
-                switch (state) {
-                    case 0:
-                        System.out.println("player " + player + "'s turn");
-                        grid.display();
-                        int x = input.nextInt();
-                        if (x > 0 && x < grid.get_xsize() + 1) {
-                            x--;
-                            int y = grid.find_y(x);
-                            if (y != -1) {
-                                if (grid.set_and_check(x, y, player)) {
-                                    state = 1;
-                                } else if (grid.draw_game()) {
-                                    state = 2;
-                                } else {
-                                    player = grid.changeplayer(player, max_players);
-                                }
+
+        while (state != -1) {
+            switch (state) {
+                case 0:
+                    System.out.println("player " + player + "'s turn");
+                    grid.display();
+                    int x = input.nextInt();
+                    if (x > 0 && x < grid.get_xsize() + 1) {
+                        x--;
+                        int y = grid.find_y(x);
+                        if (y != -1) {
+                            if (grid.set_and_check(x, y, player)) {
+                                state = 1;
+                            } else if (grid.draw_game()) {
+                                state = 2;
                             } else {
-                                System.out.println("collumn filled");
+                                player = grid.changeplayer(player, max_players);
                             }
                         } else {
-                            System.out.println("number must be > 0 < " + (grid.get_xsize() + 1));
+                            System.out.println("collumn filled");
                         }
+                    } else {
+                        System.out.println("number must be > 0 < " + (grid.get_xsize() + 1));
+                    }
 
-                        break;
-                    case 1:
+                    break;
+                case 1:
 
-                        grid.display();
-                        if (grid.draw_game()) {
-                        } else {
-                            System.out.println("\nwinner is player " + player
-                                    + "\nPlay again?\n"
-                                    + "press 0 for new game\n"
-                                    + "press anything else to quit");
-                        }
-                        if (input.nextInt() == 0) {
-                            state = 0;
-                            grid = new Grid();
-                            player = 1;
-                        } else {
-                            state = -1;
-                        }
-
-                        break;
-                    case 2:
-
-                        grid.display();
-                        System.out.println("\ndraw game"
+                    grid.display();
+                    if (grid.draw_game()) {
+                    } else {
+                        System.out.println("\nwinner is player " + player
                                 + "\nPlay again?\n"
                                 + "press 0 for new game\n"
                                 + "press anything else to quit");
+                    }
+                    if (input.nextInt() == 0) {
+                        state = 0;
+                        grid = new Grid();
+                        player = 1;
+                    } else {
+                        state = -1;
+                    }
 
-                        if (input.nextInt() == 0) {
-                            state = 0;
-                            grid = new Grid();
-                            player = 1;
-                        } else {
-                            state = -1;
-                        }
+                    break;
+                case 2:
 
-                        break;
-                }
+                    grid.display();
+                    System.out.println("\ndraw game"
+                            + "\nPlay again?\n"
+                            + "press 0 for new game\n"
+                            + "press anything else to quit");
+
+                    if (input.nextInt() == 0) {
+                        state = 0;
+                        grid = new Grid();
+                        player = 1;
+                    } else {
+                        state = -1;
+                    }
+
+                    break;
             }
         }
     }
---
+++ test/connect4/GridTest.java
@@ -0,0 +1,192 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connect4;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author jgjpro
+ */
+public class GridTest {
+
+    public GridTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Test of get_xsize method, of class Grid.
+     */
+    @Test
+    public void testGet_xsize() {
+        System.out.println("get_xsize");
+        Grid instance = new Grid();
+        int expResult = 6;
+        int result = instance.get_xsize();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of find_y method, of class Grid.
+     */
+    @Test
+    public void testFind_y() {
+        System.out.println("find_y");
+        int x = 0;
+        Grid instance = new Grid();
+        int expResult = 6;//ysize is 7 but the array address is 6
+        int result = instance.find_y(x);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of set_and_check method, of class Grid.
+     */
+    @Test
+    public void testSet_and_check_diagonal() {
+        System.out.println("set_and_check - diagonal");
+
+        Grid instance = new Grid();
+
+        int player = 1;
+        boolean win = false;
+
+        while (!win) {
+            for (int i = 0; i < 5; i++) {
+                win = instance.set_and_check(i, instance.find_y(i), player);
+                player = instance.changeplayer(player, 2);
+                if (win) {
+                    break;
+                }
+            }
+        }
+        boolean expResult = true;
+        boolean result = win;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testSet_and_check_horizontal() {
+        System.out.println("set_and_check - horizontal");
+        int player = 1;
+        int xsize = 6;
+        int ysize = 7;
+        Grid instance = new Grid();
+        boolean win = false;
+
+        for (int i = 0; i < xsize && !win; i++) {
+            for (int u = 0; u < 2; u++) {
+                win = instance.set_and_check(i, instance.find_y(i), player);
+                player = instance.changeplayer(player, 2);
+
+                if (win) {
+                    break;
+                }
+            }
+        }
+        boolean expResult = true;
+        boolean result = win;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testSet_and_check_vertical() {
+        System.out.println("set_and_check - vertical");
+        int xsize = 6;
+        int ysize = 7;
+        int player = 1;
+        Grid instance = new Grid();
+        boolean win = false;
+        while (!win) {
+            for (int i = 0; i < xsize; i++) {
+                win = instance.set_and_check(i, instance.find_y(i), player);
+                player = instance.changeplayer(player, 2);
+                if (win) {
+                    break;
+                }
+            }
+        }
+        
+        boolean expResult = true;
+        boolean result = win;
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of draw_game method, of class Grid.
+     */
+    @Test
+    public void testDraw_game() {
+        System.out.println("draw_game");
+
+        int player = 1;
+        int xsize = 6;
+        int ysize = 7;
+        boolean win = false;
+        Grid instance = new Grid();
+        for (int i = 0; i < xsize; i++) {
+            for (int j = 0; j < ysize; j++) {
+                win = instance.set_and_check(i, instance.find_y(i), player);
+                player = instance.changeplayer(player, 2);
+                if (win) {
+                    break;
+                }
+            }
+            ysize--;
+        }
+        for (int i = 0; i < xsize; i++) {
+            while (instance.find_y(i) != -1) {
+                win = instance.set_and_check(i, instance.find_y(i), player);
+                player = instance.changeplayer(player, 2);
+                if (win) {
+                    break;
+                }
+            }
+        }
+
+        boolean expResult = true;
+        boolean result = instance.draw_game();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of changeplayer method, of class Grid.
+     */
+    @Test
+    public void testChangeplayer() {
+        System.out.println("changeplayer");
+        int player = 1;
+        int max_players = 2;
+        Grid instance = new Grid();
+
+        int expResult = 2;
+        int result = instance.changeplayer(player, max_players);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}
